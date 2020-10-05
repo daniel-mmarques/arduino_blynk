1b991d522b480ce72f8a44901b127e1a63cdb307
Daniel Marques <danielmarianomarquespr@gmail.com>
2020-10-05 17:04:16 -0300

    Arquivo contendo código do projeto blynk

---
+++ blynk.ino
@@ -0,0 +1,11 @@
+void setup() {
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(2, HIGH);   
+  delay(250);                       
+  digitalWrite(2, LOW);    
+  delay(250);                       
+}
